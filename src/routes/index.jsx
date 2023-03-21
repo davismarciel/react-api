0ae37072b11d25d217081d046f04161917cab636
davismarciel <davisdevi05@gmail.com>
2023-03-21 16:48:51 -0300

    Criando rota privada

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
+import MyRoute from './MyRoute';
 import Login from '../pages/Login';
 import PageError from '../pages/404';
 
 export default function Routes() {
   return (
     <Switch>
-      <Route exact path="/" component={Login} />
-      <Route path="*" component={PageError} />
+      <MyRoute exact path="/" component={Login} />
+      <MyRoute path="*" component={PageError} />
     </Switch>
   );
 }
