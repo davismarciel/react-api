3ef6af326b4423b1ee40ac8e7a3e277efdbf0fed
davismarciel <davisdevi05@gmail.com>
2023-03-27 21:39:57 -0300

    Separando actions e reducers

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -5,7 +5,7 @@ import { useSelector } from 'react-redux';
 import { Nav } from './styled';
 
 export default function Header() {
-  const botaoClicado = useSelector((state) => state.botaoClicado);
+  const botaoClicado = useSelector((state) => state.example.botaoClicado);
   return (
     <Nav>
       <Link to="/">
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -3,15 +3,15 @@ import { useDispatch } from 'react-redux';
 import { Container } from '../../styles/GlobalStyles';
 import { Title, Paragraph } from './styled';
 
+import * as exampleActions from '../../store/modules/example/actions';
+
 export default function Login() {
   const dispatch = useDispatch();
 
   function handleClick(e) {
     e.preventDefault();
 
-    dispatch({
-      type: 'BOTAO_1',
-    });
+    dispatch(exampleActions.clicaBotao());
   }
 
   return (
